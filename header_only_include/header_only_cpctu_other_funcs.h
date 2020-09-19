658dd03c92911a1edebfcd5dd231288548e9c0a5
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2020-09-19 12:28:38 -0400

    cpctu_thread_fork and cpctu_cleanup_fork fixed.

--- header_only_include/header_only_cpctu_other_funcs.h
+++ header_only_include/header_only_cpctu_other_funcs.h
@@ -1,6 +1,7 @@
 #ifndef __cplusplus
 #ifndef Included_header_only_cpctu_other_funcs_h
 #define Included_header_only_cpctu_other_funcs_h
+#include<stdio.h>
 #include<cpctu_other_funcs.h>
 #ifdef _WIN32
 #include<synchapi.h>
@@ -22,50 +23,62 @@ struct cfh
 	cpctu_thread th;
 	struct cfh *prev;
 };
+
 // first cpctu fork helper
-struct cfh *fcfh;
+struct cfh * volatile cpctu____glv__fcfh;
+// fork status
+volatile int cpctu____glv__fs = 1;
+
 void cpctu_init_fork(void)
 {
-	fcfh = malloc(sizeof(struct cfh));
-	fcfh->prev = NULL;
-	fcfh->th = NULL;
+	cpctu____glv__fcfh = malloc(sizeof(struct cfh));
+	cpctu____glv__fcfh->prev = NULL;
+	cpctu____glv__fcfh->th = NULL;
 }
 int cpctu_fork_status(void)
 {
-	return fcfh ? 0 : 1;
+	return cpctu____glv__fcfh ? 0 : 1;
 }
 // initial function to call, fork function, inital function arg, fork function arg
 // calls the initial function, when it returns, the fork function is called on a different thread
 void cpctu_thread_fork(cpctu_func_type iftc, cpctu_func_type ff, cpctu_arg_type ifa, cpctu_arg_type ffa)
 {
+	fflush(stdout);
+	cpctu____glv__fs = 0;
 	// label to the beginning
 	lttb:
 	iftc(ifa);
 	cpctu_thread th = cpctu_create_thread(ff, ffa);
+	fflush(stdout);
 
-	if(fcfh->th == NULL)
-		fcfh->th = th;
+	if(cpctu____glv__fcfh->th == NULL)
+		cpctu____glv__fcfh->th = th;
 	else
 	{
 		struct cfh *tmp = malloc(sizeof(struct cfh));
-		fcfh->prev = fcfh;
-		fcfh->th = th;
-		fcfh = tmp;
+		cpctu____glv__fcfh->prev = cpctu____glv__fcfh;
+		cpctu____glv__fcfh->th = th;
+		cpctu____glv__fcfh = tmp;
 	}
 
-	goto lttb;
+	fflush(stdout);
+	if(cpctu____glv__fs == 0)
+		goto lttb;
 }
 void cpctu_cleanup_fork(void)
 {
-	struct cfh *tmp = fcfh;
-	while(fcfh->prev)
+	cpctu____glv__fs = 1;
+	cpctu_sleep_thread(1000);
+	struct cfh * volatile tmp = cpctu____glv__fcfh;
+	while(cpctu____glv__fcfh->prev)
 	{
-		fcfh = fcfh->prev;
+		cpctu____glv__fcfh = cpctu____glv__fcfh->prev;
+		cpctu_join_thread(tmp->th);
 		free(tmp->th);
 		free(tmp);
 	}
-	free(fcfh);
-	fcfh = NULL;
+	free(cpctu____glv__fcfh);
+	cpctu____glv__fcfh = NULL;
 }
 #endif
 #endif
